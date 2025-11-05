8062ffd8aa50e6b871ee2c0c9039bf69bd844376
VinayKumar V A <vinaykumaree5@gmail.com>
2025-11-05 15:15:18 +0000

    day-1

--- array/array_test.go
+++ array/array_test.go
@@ -176,3 +176,41 @@ func TestRotate(t *testing.T) {
 		})
 	}
 }
+
+func TestMaxProfitInt(t *testing.T) {
+	tests := []struct {
+		name   string
+		prices []int
+		want   int
+	}{
+		{
+			name:   "Example case 1",
+			prices: []int{7, 1, 5, 3, 6, 4},
+			want:   7,
+		},
+		{
+			name:   "Example case 2 - prices always decreasing",
+			prices: []int{7, 6, 4, 3, 1},
+			want:   0,
+		},
+		{
+			name:   "Empty prices",
+			prices: []int{},
+			want:   0,
+		},
+		{
+			name:   "Single element",
+			prices: []int{10},
+			want:   0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := array.MaxProfit(tt.prices)
+			if got != tt.want {
+				t.Errorf("MaxProfit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
